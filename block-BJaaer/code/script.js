81a9ae3e67cf7f2ced1442d90ce1fadcb0673379
rohitbhatt1 <rohitsharma80449@gmail.com>
2022-04-25 14:28:44 +0530

    changes

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,31 +1,48 @@
-let form =  document.querySelector("form");
+let form = document.querySelector("form");
+let model = document.querySelector(".model");
+let closeBtn = document.querySelector(".closeBtn");
+let name = document.querySelector(".name");
+let userInfo = {};
 
-let  userInfo={};
+function handleSubmit(event) {
+  event.preventDefault();
+  userInfo.name = form.elements.text.value;
+  userInfo.email = form.elements.email.value;
+  userInfo.gender = form.elements.gender.value;
+  userInfo.color = form.elements.color.value;
+  userInfo.rating = form.elements.rating.value;
+  userInfo.book = form.elements.drone.value;
+  userInfo.terms = form.elements.terms.checked;
 
-function handle(event){
-    
-    event.preventDefault();
-let data=event.target.elements;
-userInfo.name=data.name.value;
-userInfo.email=data.A.value;
-userInfo.movie= data.gender.value;
-userInfo.color =data.color.value;
-userInfo.range = data.range.value;
-userInfo.fiction = data.range.value;
-userInfo.drone = data.non-fiction.value;
-userInfo.adventure =data.adventure.value;
-userInfo.terms = data.terms.value;
+  name.innerText = `${userInfo.name}`;
+  para1 = document.createElement("p");
+  para1.innerText = `Email: ${userInfo.email}`;
+  para2 = document.createElement("p");
+  para2.innerText = `we Love: ${userInfo.gender}`;
+  para3 = document.createElement("p");
+  para3.innerText = `Color: ${userInfo.color}`;
+  para4 = document.createElement("p");
+  para4.innerText = `Rating: ${userInfo.rating}`;
+  para5 = document.createElement("p");
+  para5.innerText = `Book genre: ${userInfo.book}`;
+  small = document.createElement("small");
 
-displayUi(userInfo)
+  if (userInfo.terms === true) {
+    small.innerText = `You agreed to Terms and Conditions.`;
+  } else {
+    small.innerText = `Please agree  to Terms and Conditions!`;
+  }
+  model.append(para1, para2, para3, para4, para5, small);
+  form.style.display = "none";
+  model.style.display = "inline-block";
+  model.style.marginTop = "300px";
 }
 
-function displayUi(data){
-console.log(data)
+form.addEventListener("submit", handleSubmit);
+
+function handleClick(event) {
+  model.style.display = "none";
+  form.style.display = "inline-block";
 }
 
-
-
-
-
-
-form.addEventListener("submit",handle)
+closeBtn.addEventListener("click", handleClick);
